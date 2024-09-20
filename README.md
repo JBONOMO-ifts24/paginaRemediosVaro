ef029dc49ef97022849ea5d6618b0f1b62ce0ab0
Glitch (languid-mountain-land) <none>
2024-09-20 12:32:34 +0000

    📏🌝 Updated with Glitch

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 Web Site para la materia Desarrollo de Sistemas Web (Front End). Se presenta como trabajo práctico Integrador de la materia.
 Ahora mejorando la página para la materia BackEnd.
 
-## To-Do
+# To-Do
 
-Agregar una página que use la persistencia para mostrar mensajes.
+### + Agregar una página que use la persistencia para mostrar mensajes.
+### + Agregar usuarios a la página.
 
-## Hecho en Glitch!
+# Hecho en Glitch!
 
 [Glitch](https://glitch.com).
--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-console.log("Hola 🌎 !!!");
+document.addEventListener("DOMContentLoaded", (event) => {
+  mostrarDatos();
+  console.log("Hola 🌎 !!!");
+});
 
+function guardarDatos() {
+  const nombre = document.getElementById("nombre").value;
+  const mensaje = document.getElementById("mensaje").value;
+
+  let datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+  datosGuardados.push({ nombre, mensaje });
+
+  localStorage.setItem("datos", JSON.stringify(datosGuardados));
+
+  document.getElementById("nombre").value = "";
+  document.getElementById("mensaje").value = "";
+  mostrarDatos();
+}
+
+function mostrarDatos() {
+  const datosGuardados = JSON.parse(localStorage.getItem("datos")) || [];
+  const datosDiv = document.getElementById("datosGuardados");
+  datosDiv.innerHTML = "";
+
+  if (datosGuardados.length > 0) {
+    datosGuardados.forEach((dato, index) => {
+      const p = document.createElement("p");
+      p.innerHTML =
+        "<p> <strong> " +
+        dato.nombre +
+        "</strong> dijo: <strong>" +
+        dato.mensaje +
+        " <strong></p>";
+      datosDiv.appendChild(p);
+    });
+  } else {
+    const p2 = document.createElement("p");
+    p2.innerText = "No hay mensajes  😞";
+  }
+}
